c420725579060ca42c72189201f4d53f5873d464
wonderdip <6303226@portal.rsb.qc.ca>
2026-03-11 14:15:42 -0400

    changed end of round states

--- Scripts/StateMachine/PlayerStates/knocked.gd
+++ Scripts/StateMachine/PlayerStates/knocked.gd
@@ -4,8 +4,8 @@ class_name PlayerKnockedState
 var finished = false
 
 func enter() -> void:
+	finished = false
 	player.velocity.x = 0.0
-	player.z_index -= 2
 	player.animation_player.play(knocked_anim)
 	
 func process_physics(delta: float) -> State:
--- Scripts/StateMachine/PlayerStates/victory.gd
+++ Scripts/StateMachine/PlayerStates/victory.gd
@@ -4,13 +4,16 @@ class_name PlayerVictoryState
 var finished = false
 
 func enter() -> void:
+	finished = false
 	player.velocity.x = 0.0
+	player.z_index = 10
 	player.animation_player.play(victory_anim)
 	
 	player.animation_player.animation_finished.connect(_on_anim_finished, CONNECT_ONE_SHOT)
 
 func _on_anim_finished(_anim_name: StringName):
 	finished = true
+	player.z_index = 1
 	player.died.emit(player.other_fighter.player_number)
 	
 func process_physics(delta: float) -> State:
